e6d7700bfdf6e935eab1b098508a6a554e7ce4e8
tranvietanh0 <tranvietanh0507@gmail.com>
2024-02-25 16:56:16 +0700

    learn js

---
+++ main.js
@@ -0,0 +1,34 @@
+// phép toán 
+var a = 5;
+var b = 10;
+console.log(a + b);
+var c = 10;
+var d = '2';
+console.log(c / d);
+var res = c + a - b;
+console.log(res);
+// end phép toán
+// function 
+function sum(a, b){
+    console.log(a + b);
+}
+sum(10, 20);
+// end function 
+// object 
+var infoUser = {
+    name: "le van a",
+    age: "18",
+    email: "abc@gmail.com",
+}
+console.log(infoUser);
+// build in
+// alert("Xin chao cac b");
+// var result = confirm("Du 18 tuoi chua?");
+// console.log(result);
+// var counter = 0;
+// setInterval(function() {
+//     console.log(counter++);
+// }, 2000);
+var k = "b";
+console.log(typeof(k));
+// end build-in 
